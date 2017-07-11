896b37d28561a23d3db7bfdf7fe12db03feba097
Rick Zhang <rzhang@paperlesspost.com>
2017-07-10 22:08:13 -0400

    Updating footer

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ Name: Rick Zhang
       </div>
 
       <div id="footer">
-        The Win Application at rickzhang.cool/win isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
+        This Win Application isn't endorsed by Riot Games and doesn't reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.
       </div>
     </div>
   </body>
